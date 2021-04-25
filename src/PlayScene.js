49d22429ad8b955ebe663a0d993e5962e146f61d
Joe Alves <joseph.michael.alves@gmail.com>
2021-04-25 16:46:52 -0400

    Jumping!

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -11,13 +11,29 @@ class PlayScene extends Phaser.Scene {
     this.gameSpeed = 10;
     this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1);
 
-    setInterval(() => {
-      console.log("update");
-      this.gameSpeed = Phaser.Math.RND.integerInRange(10, 1000);
-    }, 2000);
+    // Dino Game Object (Sprite)
+    this.dino = this.physics.add.sprite(120, 0, 'dino-idle').setOrigin(0, 1);
+    /*
+     .setCollideWorldBounds(true)
+      .setGravityY(5000)
+      .setOrigin(0, 1);
+    */
+    this.dino.setCollideWorldBounds(true);
+    this.dino.setGravityY(5000);
+    // this.dino.body.bounce.y = 0.7;
 
-    this.dino = 
-      this.physics.add.sprite(120, height, 'dino-idle').setOrigin(0, 1);
+
+    this.input.keyboard.on("keydown_SPACE", () => {
+
+      // If our dino is NOT touching the ground, don't jump.
+      if (!this.dino.body.onFloor()) return;
+
+      // Set to idle image for dino.
+      this.dino.setTexture('dino', 0);
+
+      // Jump!
+      this.dino.setVelocityY(-1600);
+    });
   }
 
   update(time, delta) {
