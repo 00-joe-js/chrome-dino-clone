62f1acc5babc6d681125951b5d8b2334f63b9872
Joe Alves <joseph.michael.alves@gmail.com>
2021-04-25 17:36:37 -0400

    Cacti

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -8,11 +8,19 @@ class PlayScene extends Phaser.Scene {
 
   create() {
     const { height, width } = this.game.config;
+    this.CACTI_RESPAWN_INTERVAL = 1200;
+    this.timeSinceLastCactus = Infinity;
+    this.gameOver = false;
     this.gameSpeed = 10;
     this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1);
 
     // Dino Game Object (Sprite)
     this.dino = this.physics.add.sprite(120, 0, 'dino-idle').setOrigin(0, 1);
+    this.cactiGroup = this.physics.add.group();
+
+    this.physics.add.collider(this.dino, this.cactiGroup, () => {
+      this.gameOver = true;
+    });
     /*
      .setCollideWorldBounds(true)
       .setGravityY(5000)
@@ -22,11 +30,10 @@ class PlayScene extends Phaser.Scene {
     this.dino.setGravityY(5000);
     // this.dino.body.bounce.y = 0.7;
 
-
     this.input.keyboard.on("keydown_SPACE", () => {
 
       // If our dino is NOT touching the ground, don't jump.
-      if (!this.dino.body.onFloor()) return;
+      if (!this.dino.body.onFloor() || this.gameOver) return;
 
       // Set to idle image for dino.
       this.dino.setTexture('dino', 0);
@@ -34,10 +41,54 @@ class PlayScene extends Phaser.Scene {
       // Jump!
       this.dino.setVelocityY(-1600);
     });
+
+    this.anims.create({
+      key: 'dino-run',
+      frames: this.anims.generateFrameNumbers(
+        'dino',
+        { start: 2, end: 3 }
+      ),
+      frameRate: 10,
+      repeat: -1
+    });
+  }
+
+  putCactusOnLevel() {
+    const { width, height } = this.game.config;
+    const randomTo6 = Phaser.Math.RND.integerInRange(1, 6);
+    this.cactiGroup.add(
+      this.physics.add.sprite(
+        Phaser.Math.RND.integerInRange(width + 1, width + 500), height, `obsticle-${randomTo6}`)
+        .setOrigin(0, 1)
+        .setCollideWorldBounds()
+    );
   }
 
   update(time, delta) {
-    this.ground.tilePositionX = this.ground.tilePositionX + this.gameSpeed;
+
+    if (!this.gameOver) {
+
+      this.timeSinceLastCactus = this.timeSinceLastCactus + delta;
+      if (this.timeSinceLastCactus > this.CACTI_RESPAWN_INTERVAL) {
+        this.putCactusOnLevel();
+        this.timeSinceLastCactus = 0;
+      }
+
+      this.ground.tilePositionX = this.ground.tilePositionX + this.gameSpeed;
+
+      Phaser.Actions.IncX(this.cactiGroup.getChildren(), -this.gameSpeed);
+
+      if (this.dino.body.deltaAbsY() > 0) { // Is the dino's Y position changing?
+        this.dino.anims.stop();
+        this.dino.setTexture('dino', 0);
+      } else {
+        this.dino.play('dino-run', true);
+      }
+    } else {
+      this.dino.setTexture('dino-hurt', 0);
+    }
+
+
   }
 }
 
